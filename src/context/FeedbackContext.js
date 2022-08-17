0d332a2eb092d446fd59a1756f5d3d617727d0d0
kbiganashvili <k.biganashvili.websmart@gmail.com>
2022-08-17 10:50:09 +0400

    delete feature with JSON-Server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -54,8 +54,9 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   //Delete Feedback
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
+      await fetch(`/feedback/${id}`, { method: 'DELETE' });
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
